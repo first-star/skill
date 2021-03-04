0984afe3b3b0d57803c79e86b6cd79e112e41f2e
first-star <72744812+first-star@users.noreply.github.com>
2021-03-04 13:40:41 +0900

    アプリ完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,22 +17,25 @@ class TasksController < ApplicationController
   end
 
 
-  def edit
-    @task = Task.find(params[:id])
-  end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path, notice: "削除しました"
+    redirect_to task_path
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
 
   def update
     @task = Task.find(params[:id])
-    if @task.update(params_parameter)
-      redirect_to tasks_path, notice: "編集しました"
+    if @task.update(task_parameter)
+      redirect_to root_path, notice: "編集しました"
     else
-      render 'edit'
+      render :edit
     end
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :purpose, presence: true
+  validates :start_time, presence: true
 end
